e4b61333c09c3d32b1f4715e6db33f0b3a0c7752
Avdosh <papeverde94@gmail.com>
2021-12-20 01:38:37 +0300

    test search

---
+++ conftest.py
@@ -0,0 +1,9 @@
+import pytest
+from selenium import webdriver
+from helpers import CHROME_DRIVER_PATH
+
+@pytest.fixture()
+def fix():
+    driver = webdriver.Chrome(CHROME_DRIVER_PATH)
+    yield driver
+    driver.close()
